f4e28cd0876f14a155ada19490978a3e2de08149
Solomon <youremail@example.com>
2026-04-01 21:21:05 +0100

    fix firebase api key

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9Tf6BMss_21H1SxdWZEprMyi66GQnmHk",
+  apiKey: "AIzaSyB9Tf6BMss_21HlSxdWZEprMyi66G0nmHk",
   authDomain: "dottssa-felaco-admin.firebaseapp.com",
   projectId: "dottssa-felaco-admin",
   storageBucket: "dottssa-felaco-admin.firebasestorage.app",
   messagingSenderId: "228713849582",
-  appId: "1:228713849582:web:8cb7c4ee16c825acb709ff",
+  appId: "1:228713849582:web:8cb7c4ee16c825acb709ff"
 };
 
 const app = initializeApp(firebaseConfig);
